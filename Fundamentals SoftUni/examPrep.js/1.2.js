41e453b10deb8077c2c89933780fc702a7739e26
Grigor Yankov <109748669+YankovWeb@users.noreply.github.com>
2022-09-04 07:38:45 +0200

    Update 1.2.js
    I am formatting the code.

--- Fundamentals SoftUni/examPrep.js/1.2.js
+++ Fundamentals SoftUni/examPrep.js/1.2.js
@@ -1,46 +1,33 @@
+"use strict";
 function solve(array) {
-    let specialCustomerDiscount = 0.10
-    let taxes = 0.20
-    let flagInvalidPrice = false
-    let flagTotalIsZero = false
-    let totalPriceBeforeTaxes = 0;
-    let totalOfTaxes = 0;
-    let typeOfCustomer = array.pop();
-    let totalPrice = 0;
+  let specialCustomerDiscount = 0.1;
+  let taxes = 0.2;
+  let totalPriceBeforeTaxes = 0;
+  let totalOfTaxes = 0;
+  let typeOfCustomer = array.pop();
+  let totalPrice = 0;
 
-
-    for (let el of array) {
-        if (el < 0) {
-            console.log('Invalid price!')
-            continue;
-        }
-        totalPriceBeforeTaxes += +el
+  for (let el of array) {
+    if (el < 0) {
+      console.log("Invalid price!");
+      continue;
     }
-    totalOfTaxes += totalPriceBeforeTaxes * taxes;
-    totalPrice += totalPriceBeforeTaxes + totalOfTaxes
-    if (typeOfCustomer === 'special') {
-        totalPrice -= totalPrice * specialCustomerDiscount
-    }
+    totalPriceBeforeTaxes += +e;
+  }
+  totalOfTaxes += totalPriceBeforeTaxes * taxes;
+  totalPrice += totalPriceBeforeTaxes + totalOfTaxes;
+  if (typeOfCustomer === "special") {
+    totalPrice -= totalPrice * specialCustomerDiscount;
+  }
 
-    if (totalPriceBeforeTaxes <= 0) {
-        console.log('Invalid order!')
-    } else {
-        console.log(`Congratulations you've just bought a new computer!
+  if (totalPriceBeforeTaxes <= 0) {
+    console.log("Invalid order!");
+  } else {
+    console.log(`Congratulations you've just bought a new computer!
     Price without taxes: ${totalPriceBeforeTaxes.toFixed(2)}$
     Taxes: ${totalOfTaxes.toFixed(2)}$
     -----------
-Total price: ${totalPrice.toFixed(2)}$`)
-
-    }
-
+Total price: ${totalPrice.toFixed(2)}$`);
+  }
 }
-solve([
-    '1023',
-    '15',
-    '-20',
-    '-5.50',
-    '450',
-    '20',
-    '17.66',
-    '19.30', 'regular'
-])
+solve(["1023", "15", "-20", "-5.50", "450", "20", "17.66", "19.30", "regular"]);
